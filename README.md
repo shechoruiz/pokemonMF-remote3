b7c08e40402c1ed1ec944087b97bb229cfa9cb4d
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2024-04-14 20:30:29 -0500

    🎉 Initial commit

--- README.md
+++ README.md
@@ -2,7 +2,13 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First, declare the follow sentence in terminal:
+
+```bash
+export NEXT_PRIVATE_LOCAL_WEBPACK=true
+```
+
+Second, run the development server:
 
 ```bash
 npm run dev
--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "pokeremote3",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: {
+          pokehost:
+            "pokehost@http://localhost:3000/_next/static/chunks/remoteEntry.js",
+        },
+        exposes: {
+          "./Pokemon3": "./components/Pokemon3",
+        },
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
